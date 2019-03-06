ba1a5a891544a88efd7f84ef875c2e5e11046096
vedkirank <47411536+vedkirank@users.noreply.github.com>
2019-03-06 15:49:55 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,3 @@
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data);
-    });
-});
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const loc = document.querySelector('#location');
@@ -11,7 +5,7 @@ const forecast = document.querySelector('#forecast');
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault();
     const location = search.value;
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if(data.error){
                 loc.textContent = data.error;
--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs = require('hbs');
 const geocode = require('./utils/geocode.js');
 const weather = require('./utils/weather.js');
 const app = express();
+const port = process.env.PORT || 3000;
 
 //Define paths for express config
 
@@ -94,4 +95,6 @@ app.get('*',(req,res) => {
     });
 });
 
-app.listen(3000);
+app.listen(port,() => {
+    console.log('Server is up on port :'+port);
+});
